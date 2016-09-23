7eee5ecf7096e610e35099b7735527a453049e78
Gijsjan <agijsbro@gmail.com>
2016-09-23 08:22:45 +0200

    Extract form

---
+++ src/client/components/add-event/form.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+const Input = require('hire-forms-input').default;
+const Select = require('hire-forms-select').default;
+const AutoCompleteList = require('hire-forms-autocomplete-list').default;
+import {extractTo, extractFrom, proportionalDate} from '../../utils/dates';
+import {getEventTypes} from '../../actions/api';
+
+class Form extends React.Component<any, any> {
+	public handleChangeEventType = (value) => {
+		const { root } = this.props;
+
+		const keyValues = (value === 'Point in time') ?
+			{
+				date: proportionalDate(root, 0.5),
+				dateRange: null,
+				dateRangeUncertain: null,
+				dateUncertain: null,
+				isInterval: false,
+			} :
+			{
+				date: null,
+				dateRange: {
+					from: proportionalDate(root, 0.45),
+					to: proportionalDate(root, 0.55),
+				},
+				dateRangeUncertain: null,
+				dateUncertain: null,
+				isInterval: true,
+			};
+
+		this.props.setEventKeyValues(keyValues);
+	};
+
+	public render() {
+		const { event, setEventKeyValues } = this.props;
+
+		return (
+			<div className="form">
+				<Select
+					onChange={this.handleChangeEventType}
+					value={event.isInterval ? 'Interval of time' : 'Point in time'}
+					options={['Point in time', 'Interval of time']}
+				/>
+				<AutoCompleteList
+					async={getEventTypes}
+					onChange={(values) => {
+							setEventKeyValues({
+								types: values.map((v) => v.value),
+							});
+						}}
+					values={event.types.map((t) => ({key: t, value: t}))}
+				/>
+				<Input
+					onChange={(from: string) => console.log()}
+					value={extractFrom(event).toISOString()}
+				/>
+				{
+					(event.isInterval) ?
+						<Input
+							onChange={(to: string) => console.log()}
+							value={extractTo(event).toISOString()}
+						/> :
+						null
+				}
+				<div className="uncertain">
+					<Input
+						value={
+							event.isInterval ?
+								event.dateRangeUncertain.from :
+								event.dateUncertain.from
+						}
+					/>
+					<Input
+						value={
+							event.isInterval ?
+								event.dateRangeUncertain.to :
+								event.dateUncertain.to
+						}
+					/>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Form;
--- src/client/components/add-event/new-event.tsx
+++ src/client/components/add-event/new-event.tsx
@@ -1,15 +1,7 @@
 import * as React from 'react';
 import history from '../../routes/history';
-import {
-	proportionalDate,
-	extractFrom,
-	extractTo,
-} from '../../utils/dates';
 import Slider from './slider';
-import { getEventTypes } from '../../actions/api';
-const Input = require('hire-forms-input').default;
-const Select = require('hire-forms-select').default;
-const AutocompleteList = require('hire-forms-autocomplete-list').default;
+import Form from "./form";
 
 interface INewEventProps {
 	newEvent: IEvent;
@@ -20,30 +12,6 @@ interface INewEventProps {
 }
 
 class NewEvent extends React.Component<INewEventProps, {}> {
-	public handleChangeEventType = (value) => {
-		const { root } = this.props;
-
-		const keyValues = (value === 'Point in time') ?
-			{
-				date: proportionalDate(root, 0.5),
-				dateRange: null,
-				dateRangeUncertain: null,
-				dateUncertain: null,
-				isInterval: false,
-			} :
-			{
-				date: null,
-				dateRange: {
-					from: proportionalDate(root, 0.45),
-					to: proportionalDate(root, 0.55),
-				},
-				dateRangeUncertain: null,
-				dateUncertain: null,
-				isInterval: true,
-			};
-
-		this.props.setEventKeyValues(keyValues);
-	};
 
 	public render() {
 		const {
@@ -63,34 +31,11 @@ class NewEvent extends React.Component<INewEventProps, {}> {
 						setEventKeyValues={setEventKeyValues}
 					/>
 				</div>
-				<div className="form">
-					<Select
-						onChange={this.handleChangeEventType}
-						value={newEvent.isInterval ? 'Interval of time' : 'Point in time'}
-						options={['Point in time', 'Interval of time']}
-					/>
-					<AutocompleteList
-						async={getEventTypes}
-						onChange={(values) => {
-							setEventKeyValues({
-								types: values.map((v) => v.value),
-							});
-						}}
-						values={newEvent.types.map((t) => ({key: t, value: t}))}
-					/>
-					<Input
-						onChange={(from: string) => console.log()}
-						value={extractFrom(newEvent).toISOString()}
-					/>
-					{
-						(newEvent.isInterval) ?
-							<Input
-								onChange={(to: string) => console.log()}
-								value={extractTo(newEvent).toISOString()}
-							/> :
-							null
-					}
-				</div>
+				<Form
+					event={newEvent}
+					root={root}
+					setEventKeyValues={setEventKeyValues}
+				/>
 				<footer>
 					<button
 						onClick={() => {
